bf4ad24ed49a5b60a804a14c300691a9b1215a46
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-06-01 19:18:49 +0300

    feat: Fetch dog images on website load

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, {useEffect, useState} from 'react'
+
+function App() {
+    const [imageLink, setImageLink] = useState('')
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(response => response.json())
+            .then(data => setImageLink(data.message))
+            .catch(error => console.log(`Error fetching dog image ${error}`))
+    }, [])
+
+    if(!imageLink) return <p>Loading...</p>
+
+  return (
+    <img src={imageLink} alt='A Random Dog'></img>
+  )
+}
+
+export default App
